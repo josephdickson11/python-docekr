2084db1200f2d719306701b89d2d5813ff84eae9
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-08-09 19:36:00 +0100

    build docker image

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Flask API in Docker Container Here!'
+    return 'Hello, Flask API in Docker Container Here! and one silly lady is watching'
